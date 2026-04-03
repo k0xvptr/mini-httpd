dfe6bdb11f47d6bdaca2e5235c438813d46ea533
k0xvptr <kingsleyleoncs@gmail.com>
2026-04-03 10:55:30 -0400

    feat: Implemented a state machine that can parse GET method and rejects malformed requests

--- src/handler.c
+++ src/handler.c
@@ -1,5 +1,4 @@
-#define HEADER_PHASE 0
-#define BODY_PHASE 1
+#define MAX_METHODLEN 8
 
 #include "../include/handler.h"
 #include <errno.h>
@@ -12,50 +11,116 @@
 
 // A struct that stores data that are header related
 typedef struct {
-  char method[8];
-  char path[256];
-  char version[16];
+  char *header_buff;
+  int buffcnt;
+  // Method 
+  char *method_start;
+  char *method_end;
+  // URL
+  char* url_start;
+  char* url_end;
+} __attribute__((packed)) http_header;
 
-} http_header;
+typedef enum {
+  STATE_START = 1,
+  STATE_METHOD,
+  STATE_URL,
+  STATE_PARSE,
+  STATE_ERROR = -1,
+  STATE_COMPLETE = 0,
+} http_states;
 
 void handle_client(int sockfd, struct sockaddr_in *caddr) {
-  http_header data_header;
-  const char *term = "\r\n\r\n";
-  char buff[4096]; // Initialize a buffer for HTTP client request(s)
-  int phase = HEADER_PHASE;
-  int term_index =
-      -1; // term_index is for the current pos of the terminator "\r\n\r\n" (only for HTTP/1.x)
-  while (!phase) {
-    ssize_t sz = recv(sockfd, buff, sizeof(buff),
-                      0); // Used recv() to read HTTP client request data from kernel buffer
-    // Debugging: Show type of error using errno global variable
-    if (sz == -1) {
-      printf("Error: %d\n", errno);
-      exit(errno);
+  char buff[1024];
+  const char *delimiter = "\r\n\r\n";
+  http_header *content;
+  content->header_buff = (char *)malloc(10000 * sizeof(char));
+  content->method_start = 0;
+  content->buffcnt = 0;
+  http_states state = STATE_START;
+  while (state != STATE_COMPLETE && state != STATE_ERROR && content->buffcnt < sizeof(content->header_buff)) {
+    char* fake_req = "GET ./index.html HTTP/1.1\r\n";
+    strcpy(buff, fake_req);
+    int bytes = strlen(fake_req);
+    if (bytes <= 0) {
+      printf("error\n");
     }
-    if (sz == 0) {
-      perror("Cannot connect to client");
-      exit(errno);
-    }
+    for (int i = 0; i < bytes; ++i) {
+      switch (state) {
+      case STATE_START:
+        switch (buff[i]) {
+        case 'D':
+        case 'H':
+        case 'O':
+        case 'C':
+        case 'T':
+        case 'G':
+        case 'P':
+          content->header_buff[content->buffcnt] = buff[i];
+          content->buffcnt++;
+          state = STATE_METHOD;
+          break;
+        default:
+          state = STATE_ERROR;
+          break;
+        }
+        break;
+      case STATE_METHOD:
+        if (buff[i] != ' ') {
+          content->header_buff[content->buffcnt] = buff[i];
+          content->buffcnt++;
+        } else {
+          switch (content->header_buff[0]) {
+          case 'G':
+            if (memcmp(content->header_buff, "GET", 3) == 0) {
+              content->method_start = content->header_buff;
+              content->method_end = &content->header_buff[content->buffcnt];
+              state = STATE_URL;
+              break;
+            } else {
+              state = STATE_ERROR;
+              break;
+            }
+          case 'P':
+            char *list[] = {"POST", "PUT"};
+            for (int it = 0; it < 2; ++i) {
+              if (memcmp(content->header_buff, list[it], strlen(list[it])) == 0) {
+                content->method_start = content->header_buff;
+                content->method_end = content->header_buff + strlen(list[it]);
+                state = STATE_URL;
+                break;
+              }
+            }
+          }
+        }
+        break;
 
-    for (int n = 0; n < sz; ++n) {
+      case STATE_URL:
+        if (content->header_buff[content->buffcnt] == ' ') {
+          content->buffcnt++;
+          content->url_start = &content->header_buff[content->buffcnt];
+        } else {
+          if (buff[i] == ' ') {
+            content->buffcnt++;
+            content->url_end = &content->header_buff[content->buffcnt];
+          } else {
+            content->buffcnt++;
+          }
+        }
+      case STATE_PARSE:
 
-      // Check error states for buff[n]
-      if (buff[n] != '\r' || buff[n] != '\n') {
-        term_index = -1;
-      } else if (buff[n] == '\r' && term_index < 0) {
-        term_index = 0;
-
-        // Check valid states for buff[n]
-      } else if ((buff[n] == '\r' || buff[n] == '\n') && term_index < 3) {
-        term_index++;
-      } else {
-        phase = BODY_PHASE;
+      case STATE_ERROR:
+        printf("Error\n");
+      case STATE_COMPLETE:
+        printf("Complete\n");
         break;
       }
     }
   }
+  free(content);
+}
 
-  // TASK FOR TOMORROW: CODE THAT ENABLES ME TO SEPERATE HEADER FROM BODY CONTENT FROM CLIENT HTTP
-  // REQUEST
+int main() {
+  handle_client(0, NULL);
+  return 0;
 }
