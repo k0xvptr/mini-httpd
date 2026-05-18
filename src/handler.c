3957de7e487b171ed59e428aca39c8f13d17944f
k0xvptr <kingsleyleoncs@gmail.com>
2026-05-18 12:57:04 -0400

    refactor: Function pointers version of the HTTP server (in progress)

--- src/handler.c
+++ src/handler.c
@@ -2,49 +2,22 @@
 #define MAX_URLLEN 6000
 
 #include "../include/handler.h"
-#include "../include/url_fns.h"
-#include <errno.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <unistd.h>
+#include "../include/s_functions.h"
 
-// A struct that stores data that are header related
-typedef struct {
-  char *header_buff;
-  int cnt;
-  // Method 
-  char *method_start;
-  char *method_end;
-  // URL
-  char* url_start;
-  char* path_end;
-  char* qstr;
-  char* url_end;
-} __attribute__((packed)) http_header;
-
-typedef enum {
-  STATE_START = 1,
-  STATE_METHOD,
-  STATE_URL,
-  STATE_URL_PARSE,
-  STATE_DELIMITER,
-  STATE_ERROR = -1,
-  STATE_COMPLETE = 0,
-} http_states;
+typedef http_states (*state_fn)(http_header* header, char byte);
 
 void handle_client(int sockfd, struct sockaddr_in *caddr) {
   char buff[1024];
-  const char *delimiter = "\r\n\r\n";
-  int dcnt = 0;
-  http_header *content;
-  content->header_buff = (char *)malloc(10000 * sizeof(char));
+  const char delimiter[4] = "\r\n\r\n";
+  state_fn s_functions[3] = {
+    method, url, version
+  };
+  http_header *content = malloc(sizeof(http_header));
+  content->header_buff = (char *)malloc(100000 * sizeof(char));
   content->method_start = 0;
   content->cnt = 0;
-  http_states state = STATE_START;
-  while (state != STATE_COMPLETE && state != STATE_ERROR && content->cnt < sizeof(content->header_buff)) {
+  http_states state = STATE_METHOD;
+  while (state != STATE_COMPLETE && state != STATE_ERROR) {
     char* fake_req = "GET /index.html HTTP/1.1\r\n";
     strcpy(buff, fake_req);
     int bytes = strlen(fake_req);
@@ -52,86 +25,9 @@ void handle_client(int sockfd, struct sockaddr_in *caddr) {
       printf("error\n");
     }
     for (int i = 0; i < bytes; ++i) {
-      switch (state) {
-        case STATE_START:
-          switch (buff[i]) {
-            case 'D':
-            case 'H':
-            case 'O':
-            case 'C':
-            case 'T':
-            case 'G':
-            case 'P':
-              content->header_buff[content->cnt] = buff[i];
-              content->cnt++;
-              state = STATE_METHOD;
-              break;
-            default:
-              state = STATE_ERROR;
-              break;
-          }
-          break;
-        case STATE_METHOD:
-          if (buff[i] != ' ') {
-            content->header_buff[content->cnt] = buff[i];
-            content->cnt++;
-          } else {
-            switch (content->header_buff[0]) {
-              case 'G':
-                if (memcmp(content->header_buff, "GET", 3) == 0) {
-                  content->method_start = content->header_buff;
-                  content->method_end = &content->header_buff[content->cnt];
-                  state = STATE_URL;
-                  break;
-                } else {
-                  state = STATE_ERROR;
-                  break;
-                }
-              case 'P':
-                __asm__ volatile ("nop");
-                char *list[] = {"POST", "PUT"};
-                for (int it = 0; it < 2; ++i) {
-                  if (memcmp(content->header_buff, list[it], strlen(list[it])) == 0) {
-                    content->method_start = content->header_buff;
-                    content->method_end = content->header_buff + strlen(list[it]);
-                    state = STATE_URL;
-                    break;
-                  }
-                }
-            }
-          }
-          break;
-
-        case STATE_URL:
-          if (content->header_buff[content->cnt] == ' ') {
-            content->cnt++;
-            content->url_start = &content->header_buff[content->cnt];
-          } else {
-            if (content->cnt >= MAX_URLLEN) {
-              state = STATE_ERROR;
-            }
-            if (buff[i] == ' ') {
-              content->cnt++;
-              content->url_end = &content->header_buff[content->cnt];
-              state = STATE_URL_PARSE;
-            } else {
-              content->cnt++;
-            }
-          }
-        case STATE_URL_PARSE:
-          __asm__ volatile ("nop");
-          void (*handler[2])(int) = { game, pdf };
-        case STATE_DELIMITER:
-               
-        case STATE_ERROR:
-          printf("Error\n");
-        case STATE_COMPLETE:
-          printf("Complete\n");
-          break;
-      }
+       state = s_functions[state](content, buff[i]);  
     }
-  }
-  free(content);
+  } 
 }
 
 int main() {
---
+++ src/state-functions.c
@@ -0,0 +1,45 @@
+#include "../include/s_functions.h"
+
+#define MAXMETHODLEN 7
+
+http_states method(http_header *header, char byte) {
+  header->header_buff[header->cnt] = byte;
+  if (header->header_buff[header->cnt] == ' ') {
+    header->method_end = &header->header_buff[header->cnt];
+    header->cnt++;
+    return STATE_URL;
+  } else {
+    header->cnt++;
+    return STATE_METHOD;
+  }
+}
+
+http_states url(http_header *header, char byte) {
+  header->header_buff[header->cnt] = byte;
+  header->url_start = &header->header_buff[header->cnt];
+  if (header->header_buff[header->cnt] == ' ') {
+    header->url_end = &header->header_buff[header->cnt];
+    header->cnt++;
+    return STATE_VERSION;
+  } else {
+    header->cnt++;
+    return STATE_URL;
+  }
+}
+
+http_states version(http_header *header, char byte) {
+  const char std_version[8] = "HTTP/1.";
+  header->vstart = &header->header_buff[header->cnt];
+  if (byte != std_version[header->vcnt]) {
+    return STATE_ERROR;
+  } else {
+    header->header_buff[header->cnt] = byte;
+    if (header->header_buff[header->cnt] == ' ') {
+      header->cnt++;
+      return STATE_HEADERS;
+    } else {
+      header->cnt++;
+      return STATE_VERSION;
+    }
+  }
+}
